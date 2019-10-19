a1d3a0224d5314df7baff231c8631b38b1935bde
ilyasbayuaji <bayu_173040156@mail.unpas.ac.id>
2019-10-18 23:58:11 -0700

    pagination

---
+++ application/controllers/People.php
@@ -0,0 +1,40 @@
+<?php
+
+class People extends CI_Controller
+{
+    public function index()
+    {
+        $data['judul'] = 'List of People';
+
+        $this->load->model('Peoples_model', 'peoples');
+
+        //load library
+        $this->load->library('pagination');
+
+        //ambil data keyword
+        if($this->->post('submit')) {
+            $data['keyword'] = $this->input->post('keyword');
+            $this->session->set_userdata('keyword, $data['keyword);
+
+        } else {
+            $data['keyword'] = $this->session->userdata('keyword');
+
+        }
+            //config
+        $this->db->like('name', $data['keyword']);
+        $this->db->like('email', $data['keyword']);
+        $this->db->from('peoples');
+        $config['per_page'] = 8;
+
+        //initialize
+        $this->pagination->initialize($config);
+
+        $data['start'] = $this->url->segment(3);
+        $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start'], $data['keyword']);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('peoples/index', $data);
+        $this->load->view('templates/footer');
+
+    }
+}
