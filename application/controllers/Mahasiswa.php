18c4f517d98b0e4334a2bd6f29cd60a2571ec738
ilyasbayuaji <bayu_173040156@mail.unpas.ac.id>
2019-10-19 00:10:40 -0700

    membenarkan mahasiswa.php

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,74 +1,76 @@
-<?php
+<?php 
 
-class Mahasiswa extends CI_Controller
-{
-    public function index()
-    {
-   
-        $this->load->model('Mahasiswa_model', 'mhs');
-        $data['mahasiswa'] = $this->mhs->getAllMahasiswa();
-      
-        $this->load->view('templates/header', $data);
-        $this->load->view('mahasiswa/index', $data);
-        $this->load->view('templates/footer');
-    }
+	class Mahasiswa extends CI_Controller {
 
-    public function tambah()
-    {
-        $data['judul'] = 'Form Tambah Data Mahasiswa';
+		public function __construct()
+		{
+			parent::__construct();
+			$this->load->model('Mahasiswa_model');
+			$this->load->library('form_validation');
+		}
 
-        $this->form_validation->set_rules('nama', 'Nama', 'required');
-        $this->form_validation->set_rules('nrp', 'Nrp', 'required|numeric');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		public function index()
+		{
+			$data['judul'] = 'Daftar Mahasiswa';
+			$data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+			$this->load->view('templates/header' , $data);
+			$this->load->view('mahasiswa/index' , $data);
+			$this->load->view('templates/footer');
+		}
 
-        if($this->form_validation->run() == false) 
-        { 
-            $this->load->view('templates/header', $data);
-            $this->load->view('mahasiswa/tambah');
-            $this->load->view('templates/footer');
-        } else {
-            $this->Mahasiswa_model->tambahDataMahasiswa();
-            $this->session->set_flashdata('flash', 'Ditambahkan');
-            redirect('mahasiswa');
-        }
-    }
+		public function tambah()
+		{
+			$data['judul'] = 'Form Tambah Data Mahasiswa';
 
-    public function hapus($id)
-    {
-        $this->Mahasiswa_model->hapusDataMahasiswa($id);
-        $this->session->set_flashdata('flash', 'Dihapus');
-        redirect('mahasiswa');
-    }
+			$this->form_validation->set_rules('nama', 'Nama', 'required');
+			$this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+			$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+			if( $this->form_validation->run() == FALSE ) {
+			$this->load->view('templates/header' , $data);
+			$this->load->view('mahasiswa/tambah');
+			$this->load->view('templates/footer');
+			} else {
+				$this->Mahasiswa_model->tambahDataMahasiswa();
+				$this->session->set_flashdata('flash', 'Ditambahkan');
+				redirect('mahasiswa');
+			}
+		}
 
-    public function detail($id)
-    {
-        $data['judul'] = 'Detail Data Mahasiswa';
-        $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
-        $this->load->view('templates/header', $data);
-        $this->load->view('mahasiswa/detail', $data);
-        $this->load->view('templates/footer');
-    }
+		public function hapus($id)
+		{
+			$this->Mahasiswa_model->hapusDataMahasiswa($id);
+			$this->session->set_flashdata('flash', 'Dihapus');
+			redirect('mahasiswa');
+		}
 
-    public function ubah($id)
-    {
-        $data['judul'] = 'Form Ubah Data Mahasiswa';
-        $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
-        $data['jurusan'] = ['Teknik Informatika', 'Teknik Mesin', 'Teknik Planologi', 'Teknik Elektro', 'Teknik Pangan', 'Teknik Lingkungan'];
+		public function detail($id)
+		{
+			$data['judul'] = 'Detail Data Mahasiswa';
+			$data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+			$this->load->view('templates/header' , $data);
+			$this->load->view('mahasiswa/detail' , $data);
+			$this->load->view('templates/footer');
+		}
 
-        $this->form_validation->set_rules('nama', 'Nama', 'required');
-        $this->form_validation->set_rules('nrp', 'Nrp', 'required|numeric');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		public function ubah($id)
+		{
+			$data['judul'] = 'Detail Data Mahasiswa';
+			$data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+			$data['jurusan'] = ['Teknik Informatika','Teknik Industri','Teknik Pangan','Teknik Mesin','Teknik Planologi','Teknik Lingkungan'];
 
-        if($this->form_validation->run() == false) 
-        { 
-            $this->load->view('templates/header', $data);
-            $this->load->view('mahasiswa/ubah', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $this->Mahasiswa_model->ubahDataMahasiswa();
-            $this->session->set_flashdata('flash', 'Diubah');
-            redirect('mahasiswa');
-        }
+			$this->form_validation->set_rules('nama', 'Nama', 'required');
+			$this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+			$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
 
-    }
-}
+			if( $this->form_validation->run() == FALSE ) {
+			$this->load->view('templates/header' , $data);
+			$this->load->view('mahasiswa/ubah' , $data);
+			$this->load->view('templates/footer');
+			} else {
+				$this->Mahasiswa_model->tambahDataMahasiswa();
+				$this->session->set_flashdata('flash', 'Diubah');
+				redirect('mahasiswa');
+			}
+		}
+
+	}
